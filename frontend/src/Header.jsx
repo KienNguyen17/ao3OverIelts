7f5ce68600c4616959f3fe8dbe0ad8a3d9b2ede7
Kien Nguyen <knguyen2@macalester.edu>
2024-09-13 12:51:22 -0500

    basic setup

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+            <h1>AO3 Learner</h1>
+            <img src="" alt="logo"/>
+        </>
+    )
+}
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <>
+            <ul>
+                <li>Home</li>
+                <li>My Works</li>
+                <li>Learning</li>
+                <li>About Us</li>
+            </ul>
+        </>
+    )
+}
